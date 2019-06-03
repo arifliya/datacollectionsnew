b8e589f6dc51994cd0f13288c8c745ac54d94d5f
Arif Liya <arifliya@gmail.com>
2019-06-03 11:24:09 +0100

    mayoral reports added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,8 +59,8 @@ $('#esfRound').click(function() {
 // from myjquery file //
 
 $('.govuk-button').click(function() {
-   if($('#fix-errors').is(':checked')) { location.href='/12-0-0/choose_file_to_upload'; }
-	 else if ($('#submit-file').is(':checked')) { location.href='/12-0-0/ilr_data_submitted'; }
+   if($('#fix-errors').is(':checked')) { location.href='choose_file_to_upload'; }
+	 else if ($('#submit-file').is(':checked')) { location.href='ilr_data_submitted'; }
 
    else if(!$('.govuk-radios__item input').is(':checked')) {
      $('#formError').addClass('govuk-form-group--error');
