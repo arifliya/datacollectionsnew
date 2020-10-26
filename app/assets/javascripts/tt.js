af4a25952c83c95006547a52b5b1a321583808e1
Arif Liya <arifliya@gmail.com>
2020-10-26 12:06:23 +0000

    esfr designs added

--- app/assets/javascripts/tt.js
+++ app/assets/javascripts/tt.js
@@ -13,99 +13,18 @@ window.onload = function() {
   }
 
   function showInfo(data, tabletop) {
-    // var tr;
-    // for (var i = 0; i < data.length; i++) {
-    //   tr = $('<tr class="govuk-table__row" />');
-    //   tr.append("<td>" + data[i].amount + "</td>");
-    //   $('#fill1').append(tr);
-    // }
-    //
-    // for (var i = 0; i < data.length; i++) {
-    //   tr = $('<tr class="govuk-table__row" />');
-    //   tr.append("<td>" + data[i].amount + "</td>");
-    //   $('#fill2').append(tr);
-    // }
-    // $('#bannerText').append(data[1].banner);
-    // $('#bannerText2').append(data[2].banner);
-    // $('#bannerText3').append(data[3].banner);
-    // $('#bannerText4').append(data[4].banner);
-    // $('#bannerText5').append(data[5].banner);
-    // $('#bannerText6').append(data[6].banner);
-    // $('#bannerText7').append(data[7].banner);
-    // $('#bannerText8').append(data[8].banner);
-    // $('#bannerText9').append(data[9].banner);
-    // $('#bannerText10').append(data[10].banner);
-
-    // $('#bannerCont').append(data[1].context);
-    // $('#bannerCont2').append(data[2].context);
-    // $('#bannerCont3').append(data[3].context);
-    // $('#bannerCont4').append(data[4].context);
-    // $('#bannerCont5').append(data[5].context);
-    // $('#bannerCont6').append(data[6].context);
-    // $('#bannerCont7').append(data[7].context);
-    // $('#bannerCont8').append(data[8].context);
-    // $('#bannerCont9').append(data[9].context);
-    // $('#bannerCont10').append(data[10].context);
-
-
-    // $('#a2').append(data[0].forecasted);
-    // $('#b2').append(data[0].delivery);
-    // $('#c2').append(data[0].total);
-
-    // $('#a3').append(data[1].forecasted);
-    // $('#b3').append(data[1].delivery);
-    // $('#c3').append(data[1].total);
-
-    // $('#a4').append(data[2].forecasted);
-    // $('#b4').append(data[2].delivery);
-    // $('#c4').append(data[2].total);
-
-    // $('#a5').append(data[3].forecasted);
-    // $('#b5').append(data[3].delivery);
-    // $('#c5').append(data[3].total);
-
-    // $('#a6').append(data[4].forecasted);
-    // $('#b6').append(data[4].delivery);
-    // $('#c6').append(data[4].total);
-
-    // $('#a7').append(data[5].forecasted);
-    // $('#b7').append(data[5].delivery);
-    // $('#c7').append(data[5].total);
-
-    // $('#a8').append(data[6].forecasted);
-    // $('#b8').append(data[6].delivery);
-    // $('#c8').append(data[6].total);
-
-    // $('#a9').append(data[7].forecasted);
-    // $('#b9').append(data[7].delivery);
-    // $('#c9').append(data[7].total);
-
-    // $('#a10').append(data[8].forecasted);
-    // $('#b10').append(data[8].delivery);
-    // $('#c10').append(data[8].total);
-
-    // $('#a11').append(data[9].forecasted);
-    // $('#b11').append(data[9].delivery);
-    // $('#c11').append(data[9].total);
-
-    // $('#12').append(data[10].total);
-
-    // if (data[0].banner == '1') {
-    //   $('.information-banner').removeClass('hidden');
-    // }
-
-    // $('#returnPeriodName').text(data[0].test);
+    
 
     $('#journey').append(data[0].journey);
 
+    //************** fc ****************//
 
     if (data[0].fc == 'closed') {
       $('#fcTile').hide();
       $('#fcTileView').show();
-      $('#fcStatus').text('Opens on 5 October 2019');
-      
-      
-    } else if (data[0].fc == 'open') {
+      $('#fcStatus').text('Opens on 5 October 2019'); 
+    } 
+      else if (data[0].fc == 'open') {
       $('#fcTile').show();
       $('#fcTileView').hide();
       $('.interruption-card.hidden').show();
@@ -115,6 +34,25 @@ window.onload = function() {
       else if (data[0].fc == 'na') {
       $('#fcTile').hide();
       $('#fcTileView').hide();
+    }
+    
+    //************** esfr ****************//
+    
+    if (data[0].esfr == 'closed') {
+      $('#esfrTile').hide();
+      $('#esfrTileView').show();
+      // $('#fcStatus').text('Opens on 5 October 2019'); 
+    } 
+      else if (data[0].esfr == 'open') {
+      $('#esfrTile').show();
+      $('#esfrTileView').hide();
+      // $('.interruption-card.hidden').show();
+      // $('#fcStatus').text('Open for submission');
+      // $('#reviewAndEdit').show();
+    } 
+      else if (data[0].esfr == 'na') {
+      $('#esfrTile').hide();
+      $('#esfrTileView').hide();
     }
 
 
@@ -186,11 +124,11 @@ window.onload = function() {
         $('#esfTile').show();
       }
       
-      if (data[0].esfr == 'hide') {
-        $('#esfrTile').hide();
-      } else {
-        $('#esfrTile').show();
-      }
+      // if (data[0].esfr == 'hide') {
+      //   $('#esfrTile').hide();
+      // } else {
+      //   $('#esfrTile').show();
+      // }
     }
 
     if (data[0].journey == '1-open-period') {
@@ -223,11 +161,11 @@ window.onload = function() {
         $('#esfTile').show();
       }
 
-      if (data[0].esfr == 'hide') {
-        $('#esfrTile').hide();
-      } else {
-        $('#esfrTile').show();
-      }
+      // if (data[0].esfr == 'hide') {
+      //   $('#esfrTile').hide();
+      // } else {
+      //   $('#esfrTile').show();
+      // }
     }
  
     if (data[0].journey == '1-open-1-validation') {
@@ -277,11 +215,11 @@ window.onload = function() {
         $('#esfTile').show();
       }
       
-      if (data[0].esfr == 'hide') {
-        $('#esfrTile').hide();
-      } else {
-        $('#esfrTile').show();
-      }
+      // if (data[0].esfr == 'hide') {
+      //   $('#esfrTile').hide();
+      // } else {
+      //   $('#esfrTile').show();
+      // }
     }
 
     if (data[0].journey == '2-open-periods') {
@@ -318,11 +256,11 @@ window.onload = function() {
         $('#esfTile').show();
       }
 
-      if (data[0].esfr == 'hide') {
-        $('#esfrTile').hide();
-      } else {
-        $('#esfrTile').show();
-      }
+      // if (data[0].esfr == 'hide') {
+      //   $('#esfrTile').hide();
+      // } else {
+      //   $('#esfrTile').show();
+      // }
     }
 
 
